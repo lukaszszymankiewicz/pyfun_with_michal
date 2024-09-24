b15b8110e775dbc3848b51e1bfd3832420bb3b5c
szymankl <lukasz_szymankiewicz@op.pl>
2024-09-24 18:59:28 +0200

    unit-test added

--- tests/test_src.py
+++ tests/test_src.py
@@ -8,15 +8,19 @@ def test_set_prices_sorting_works():
     #
     result = set_prices(prices, 1.0)
 
-    # TODO: correct me!
-
-    # if result_sorted != result:
-    #     raise AssertionError()
+    #
     assert result_sorted == result
     assert result_sorted == [10, 20, 30]
 
 
 def test_set_prices_multiplying_works():
-    # TODO: write me!
+    #
+    prices = [10, 30, 20]
+    a = 2
+    expected = [20, 40, 60]
 
-    assert True
+    #
+    result = set_prices(prices, a)
+
+    #
+    assert expected == result
