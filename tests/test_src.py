910f544c7166b717bb2686fe51f9939bec815806
MichalLuczak143 <michal0127@outlook.com>
2024-09-25 12:45:58 +0200

    Request 123 - "praca domowa"

--- tests/test_src.py
+++ tests/test_src.py
@@ -1,22 +1,32 @@
 from app import set_prices
+from app import app
 
 def test_set_prices_sorting_works():
     #
     prices = [10, 30, 20]
-    result_sorted = prices.sort()
+    prices.sort()
+   # return prices
 
     #
     result = set_prices(prices, 1.0)
 
-    # TODO: correct me!
 
     # if result_sorted != result:
     #     raise AssertionError()
-    assert result_sorted == result
-    assert result_sorted == [10, 20, 30]
+    assert prices == result
+    assert prices == [10, 20, 30]
 
 
-def test_set_prices_multiplying_works():
-    # TODO: write me!
+def test_set_prices_multiplying_works(prices=[10, 20, 30], a=2):
 
-    assert True
+
+    # Obliczenie wyniku
+    expected_result = [p * a for p in prices]
+
+    # Wywołanie funkcji
+    result = set_prices(prices, a)
+
+    # Sprawdzenie
+    assert result == expected_result
+
+
